efa66065665cc1aae26ccd0d6e2d5156e64d397b
badgod86 <80606494+badgod86@users.noreply.github.com>
2021-03-15 14:39:13 -0500

    Create ssh_server.pp

---
+++ site/role/manifests/ssh_server.pp
@@ -0,0 +1,15 @@
+class profile::ssh_server {
+	package {'openssh-server':
+		ensure => present,
+	}
+	service { 'sshd':
+		ensure => 'running',
+		enable => 'true',
+	}
+	ssh_authorized_key { 'root@master.puppet.vm':
+		ensure => present,
+		user   => 'root',
+		type   => 'ssh-rsa',
+		key    => 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDc7cI4Ls9RfcnlWDFDXssphG2pIDxYlVQWcyqSbV2/09T0uMAkz6EiK/J20ZkD3IRPv+CMna+fCbgb8+nCQt0aCeHgvbZk/dbOV90+plP/YYW7UJJV0/pIRMtA5lQIJRR3g7inhpQiRTFLl4SWzDLTj48q12+9Pq9XdeMXq80KDddmYroWHyAFiKZFyt36SDt5j5ItnAGFdGVdY48+zuXvLYOFAxi0ClpgvtXSdiMXSnxWPGmMAdFqJSHhJkethiMKcShDx2X0MPUsfzxXNQzKd7ErQQKMZS2X0pX4bQGvz8pkqEE/nebWnBj7xYaFVpYPGbV7VHnFkGcOs82Py6HV root@master.puppet.vm',
+	}  
+}
